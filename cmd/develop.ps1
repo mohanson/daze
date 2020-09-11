59259954d6ccec33bb3656823ee81c41d002910d
mohanson <mohanson@outlook.com>
2020-09-11 18:47:44 +0800

    Submit for stash 03

---
+++ cmd/develop.ps1
@@ -0,0 +1,13 @@
+if (!(Test-Path ./bin)) {
+    New-Item -Path ./bin -ItemType Directory
+}
+
+if (!(Test-Path ./bin/delegated-apnic-latest)) {
+    Invoke-WebRequest -Uri http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -OutFile ./bin/delegated-apnic-latest
+}
+
+if (!(Test-Path ./bin/rule.ls)) {
+    Copy-Item ./res/rule.ls -Destination ./bin/rule.ls
+}
+
+& go build -o bin github.com/mohanson/daze/cmd/daze
