dad4379e5af15b60ec8f37701ebafd21cda1da11
mohanson <mohanson@outlook.com>
2022-02-22 23:01:03 +0800

    2022-02-22 23:01:03

--- cmd/release.sh
+++ cmd/release.sh
@@ -2,11 +2,10 @@ set -ex
 
 rm -rf ./bin/release
 mkdir ./bin/release
-wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -O ./bin/release/delegated-apnic-latest
 
 make(){
     mkdir ./bin/release/daze_$1_$2
-    cp ./bin/release/delegated-apnic-latest ./bin/release/daze_$1_$2/delegated-apnic-latest
+    cp ./res/rule.cidr ./bin/release/daze_$1_$2/rule.cidr
     cp ./res/rule.ls ./bin/release/daze_$1_$2/rule.ls
     GOOS=$1 GOARCH=$2 go build -o ./bin/release/daze_$1_$2 github.com/mohanson/daze/cmd/daze
     python3 -m zipfile -c ./bin/release/daze_$1_$2.zip ./bin/release/daze_$1_$2
@@ -17,5 +16,3 @@ make(){
 make linux amd64
 make linux arm64
 make windows amd64
-
-rm ./bin/release/delegated-apnic-latest
