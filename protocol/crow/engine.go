0d3297868e6c37e0978c566b846d841640dc4633
mohanson <mohanson@outlook.com>
2022-07-08 01:36:57 +0800

    2022-07-08 01:36:57

--- protocol/crow/engine.go
+++ protocol/crow/engine.go
@@ -376,7 +376,7 @@ func (c *Client) Run() {
 				headerMsgLen = binary.BigEndian.Uint16(buf[3:5])
 				doa.Doa(headerMsgLen <= 2040)
 				buf[0] = 2
-				c.Lio.Write(buf[:8+headerMsgLen])
+				srv.Write(buf[:8+headerMsgLen])
 			case 2:
 				headerIdx = binary.BigEndian.Uint16(buf[1:3])
 				headerMsgLen = binary.BigEndian.Uint16(buf[3:5])
