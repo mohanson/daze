67ee2e3426380b94a83e35d2d734723f7baba27f
mohanson <mohanson@outlook.com>
2022-08-02 18:36:02 +0800

    Update release.sh

--- cmd/release.sh
+++ cmd/release.sh
@@ -1,16 +1,16 @@
 set -ex
 
 rm -rf ./bin/release
-mkdir ./bin/release
+mkdir -p ./bin/release
 
-make(){
+make() {
     mkdir ./bin/release/daze_$1_$2
     cp ./README.md ./bin/release/daze_$1_$2/README.md
     cp ./README_CN.md ./bin/release/daze_$1_$2/README_CN.md
     cp ./res/rule.cidr ./bin/release/daze_$1_$2/rule.cidr
     cp ./res/rule.ls ./bin/release/daze_$1_$2/rule.ls
     GOOS=$1 GOARCH=$2 go build -o ./bin/release/daze_$1_$2 github.com/mohanson/daze/cmd/daze
-    python3 -m zipfile -c ./bin/release/daze_$1_$2.zip ./bin/release/daze_$1_$2
+    python -m zipfile -c ./bin/release/daze_$1_$2.zip ./bin/release/daze_$1_$2
 }
 
 # https://golang.org/doc/install/source#environment
