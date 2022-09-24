a31a08e12bea123a1670a8d4b790d7ec66ca25b3
mohanson <mohanson@outlook.com>
2022-09-24 20:14:08 +0800

    Czar protocol

---
+++ protocol/czar/engine.go
@@ -0,0 +1,166 @@
+package czar
+
+import (
+	"errors"
+	"io"
+	"log"
+	"math"
+	"net"
+
+	"github.com/mohanson/daze"
+	"github.com/mohanson/daze/protocol/ashe"
+)
+
+// The czar protocol is a proxy protocol built on TCP multiplexing technology. By establishing multiple TCP connections
+// in one TCP channel, czar protocol effectively reduces the consumption of establishing connections between the client
+// and the server:
+//
+// Client port: a.com ------------┐                   ┌------------ Server port: a.com
+// Client port: b.com ----------┐ |                   | ┌---------- Server port: b.com
+// Client port: c.com ----------+-+-- czar protocol --+-+---------- Server port: c.com
+// Client port: d.com ----------┘ |                   | └---------- Server port: d.com
+// Client port: e.com ------------┘                   └------------ Server port: e.com
+//
+// To open a stream:
+//
+// +-----+-----+-----+-----+
+// | Sid |  0  |    Rsv    |
+// +-----+-----+-----+-----+
+//
+// Both server and client can push data to each other.
+//
+// +-----+-----+-----+-----+-----+-----+
+// | Sid |  1  |    Len    |    Msg    |
+// +-----+-----+-----+-----+-----+-----+
+//
+// Close the specified stream.
+//
+// +-----+-----+-----+-----+
+// | Sid |  2  |    Rsv    |
+// +-----+-----+-----+-----+
+
+// Server implemented the czar protocol.
+type Server struct {
+	Listen string
+	Cipher []byte
+	Closer io.Closer
+}
+
+// Serve incoming connections. Parameter cli will be closed automatically when the function exits.
+func (s *Server) Serve(ctx *daze.Context, cli io.ReadWriteCloser) error {
+	asheServer := &ashe.Server{Cipher: s.Cipher}
+	return asheServer.Serve(ctx, cli)
+}
+
+// Close listener.
+func (s *Server) Close() error {
+	if s.Closer != nil {
+		return s.Closer.Close()
+	}
+	return nil
+}
+
+// Run it.
+func (s *Server) Run() error {
+	ln, err := net.Listen("tcp", s.Listen)
+	if err != nil {
+		return err
+	}
+	s.Closer = ln
+	log.Println("listen and serve on", s.Listen)
+
+	go func() {
+		idx := uint32(math.MaxUint32)
+		for {
+			cli, err := ln.Accept()
+			if err != nil {
+				if !errors.Is(err, net.ErrClosed) {
+					log.Println(err)
+				}
+				break
+			}
+			mux := NewMuxServer(cli)
+			go func(mux *Mux) {
+				defer mux.Close()
+				for cli := range mux.Accept {
+					idx++
+					ctx := &daze.Context{Cid: idx}
+					log.Printf("%08x accept remote=%s", ctx.Cid, mux.Conn.RemoteAddr())
+					go func(cli io.ReadWriteCloser) {
+						defer cli.Close()
+						if err := s.Serve(ctx, cli); err != nil {
+							log.Printf("%08x  error %s", ctx.Cid, err)
+						}
+						log.Printf("%08x closed", ctx.Cid)
+					}(cli)
+				}
+			}(mux)
+		}
+	}()
+
+	return nil
+}
+
+// NewServer returns a new Server.
+func NewServer(listen string, cipher string) *Server {
+	return &Server{
+		Listen: listen,
+		Cipher: daze.Salt(cipher),
+	}
+}
+
+// Client implemented the czar protocol.
+type Client struct {
+	Cipher []byte
+	Server string
+	Mux    chan *Mux
+}
+
+func (c *Client) Close() error {
+	return nil
+}
+
+// Dial connects to the address on the named network.
+func (c *Client) Dial(ctx *daze.Context, network string, address string) (io.ReadWriteCloser, error) {
+	mux := <-c.Mux
+	srv, err := mux.Open()
+	if err != nil {
+		return nil, err
+	}
+	asheClient := &ashe.Client{Cipher: c.Cipher}
+	ret, err := asheClient.With(ctx, srv, network, address)
+	if err != nil {
+		srv.Close()
+	}
+	return ret, err
+}
+
+// Run creates an establish connection to czar server.
+func (c *Client) Run() {
+L1:
+	srv, err := daze.Dial("tcp", c.Server)
+	if err != nil {
+		goto L1
+	}
+	mux := NewMuxClient(srv)
+L2:
+	select {
+	case c.Mux <- mux:
+		goto L2
+	case <-mux.RecvDone:
+		goto L1
+	case <-mux.SendDone:
+		goto L1
+	}
+}
+
+// NewClient returns a new Client.
+func NewClient(server, cipher string) *Client {
+	client := &Client{
+		Cipher: daze.Salt(cipher),
+		Server: server,
+		Mux:    make(chan *Mux),
+	}
+	go client.Run()
+	return client
+}
