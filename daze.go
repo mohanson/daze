bcf14ba6f248193a371af2b6a6440e24d821810c
mohanson <mohanson@outlook.com>
2023-06-11 18:10:08 +0800

    Append microsoft DNS

--- daze.go
+++ daze.go
@@ -1053,6 +1053,8 @@ func LoadOpenResolver() []string {
 	return []string{
 		"8.8.8.8:53",        // Google
 		"8.8.4.4:53",        // Google
+		"4.2.2.1:53",        // Microsoft
+		"4.2.2.2:53",        // Microsoft
 		"1.1.1.1:53",        // Cloudflare DNS
 		"1.0.0.1:53",        // Cloudflare DNS
 		"208.67.222.222:53", // OpenDNS
