4d4d7c234d4299698f2e9c6b886eaccd17499cbd
mohanson <mohanson@outlook.com>
2024-04-06 11:22:30 +0800

    Rewrite package lru test

--- lib/lru/lru_test.go
+++ lib/lru/lru_test.go
@@ -1,77 +1,74 @@
 package lru
 
 import (
-	"math/rand/v2"
 	"testing"
 )
 
 func TestLruAppend(t *testing.T) {
-	c := New[string, int](4)
-	c.Set("a", 1)
-	c.Set("b", 2)
-	c.Set("c", 3)
-	c.Set("d", 4)
-	c.Set("e", 5)
-	if c.Get("a") != 0 {
+	c := New[int, int](4)
+	c.Set(1, 1)
+	c.Set(2, 2)
+	c.Set(3, 3)
+	c.Set(4, 4)
+	c.Set(5, 5)
+	if c.Get(1) != 0 {
 		t.FailNow()
 	}
-	if c.Get("e") != 5 {
+	if c.Get(5) != 5 {
 		t.FailNow()
 	}
 }
 
 func TestLruChange(t *testing.T) {
-	c := New[string, int](4)
-	c.Set("a", 1)
-	c.Set("b", 2)
-	c.Set("c", 3)
-	c.Set("d", 4)
-	c.Set("a", 5)
-	if c.Get("a") != 5 {
+	c := New[int, int](4)
+	c.Set(1, 1)
+	c.Set(2, 2)
+	c.Set(3, 3)
+	c.Set(4, 4)
+	c.Set(1, 5)
+	if c.Get(1) != 5 {
 		t.FailNow()
 	}
 }
 
 func TestLruDel(t *testing.T) {
-	c := New[string, int](4)
-	c.Set("a", 1)
-	c.Set("b", 2)
-	c.Set("c", 3)
-	c.Set("d", 4)
-	c.Del("b")
+	c := New[int, int](4)
+	c.Set(1, 1)
+	c.Set(2, 2)
+	c.Set(3, 3)
+	c.Set(4, 4)
+	c.Del(2)
 	if c.List.Size != c.Len() || c.Len() != 3 {
 		t.FailNow()
 	}
-	if c.Get("b") != 0 {
+	if c.Get(2) != 0 {
 		t.FailNow()
 	}
 }
 
 func TestLruSize(t *testing.T) {
-	c := New[uint64, uint64](4)
+	c := New[int, int](4)
 	if c.List.Size != c.Len() || c.Len() != 0 {
 		t.FailNow()
 	}
-	c.Set(rand.Uint64(), rand.Uint64())
+	c.Set(1, 1)
 	if c.List.Size != c.Len() || c.Len() != 1 {
 		t.FailNow()
 	}
-	c.Set(rand.Uint64(), rand.Uint64())
+	c.Set(2, 2)
 	if c.List.Size != c.Len() || c.Len() != 2 {
 		t.FailNow()
 	}
-	c.Set(rand.Uint64(), rand.Uint64())
+	c.Set(3, 3)
 	if c.List.Size != c.Len() || c.Len() != 3 {
 		t.FailNow()
 	}
-	c.Set(rand.Uint64(), rand.Uint64())
+	c.Set(4, 4)
 	if c.List.Size != c.Len() || c.Len() != 4 {
 		t.FailNow()
 	}
-	for range 65536 {
-		c.Set(rand.Uint64(), rand.Uint64())
-		if c.List.Size != c.Len() || c.Len() != 4 {
-			t.FailNow()
-		}
+	c.Set(5, 5)
+	if c.List.Size != c.Len() || c.Len() != 4 {
+		t.FailNow()
 	}
 }
