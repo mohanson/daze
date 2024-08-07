209ef4dfed0a5609f4167b4a449d33a3dcdedbf5
mohanson <mohanson@outlook.com>
2024-08-07 17:35:08 +0800

    Rewrite the gracefulexit by using os-independent signals

--- lib/gracefulexit/gracefulexit.go
+++ lib/gracefulexit/gracefulexit.go
@@ -6,13 +6,12 @@ package gracefulexit
 import (
 	"os"
 	"os/signal"
-	"syscall"
 )
 
 // Chan create a channel for os.Signal.
 func Chan() chan os.Signal {
 	buffer := make(chan os.Signal, 1)
-	signal.Notify(buffer, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(buffer, os.Interrupt)
 	return buffer
 }
 
